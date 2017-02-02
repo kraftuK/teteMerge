b7126e0436751c1e467170dad8b891c5d5cccee4
Evgeny Erofeev <kraftu@kilograpp.com>
2017-02-02 16:30:08 +0300

    Добавил еще один коммит

--- app/src/main/java/com/kilograpp/testmerge/MainActivity.java
+++ app/src/main/java/com/kilograpp/testmerge/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //Тут переход на экран
+                //TT
             }
         });
 
